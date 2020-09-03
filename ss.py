e14198f3fc1c8584c2c043822e6b317f374d58d3
kaleb hunter <65514123+kalebhunter@users.noreply.github.com>
2020-09-04 09:46:48 +1200

    Add files via upload

---
+++ ss.py
@@ -0,0 +1,17 @@
+import turtle
+
+yeet = turtle.Turtle()
+
+yeet.forward(100)
+yeet.right(90)
+
+yeet.forward(100)
+yeet.right(90)
+
+yeet.forward(100)
+yeet.right(90)
+
+yeet.forward(100)
+yeet.right(90)
+
+turtle.done()
